bfd63309a4470c6cdb27678bb92ea2460ab36907
Alexey Lagun <alexey.lagun@gmail.com>
2022-12-06 19:18:03 +0300

    feat: solution getMovingSum

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -226,8 +226,13 @@ function toArrayOfSquares(arr) {
  *   [ 0, 0, 0, 0, 0]         => [ 0, 0, 0, 0, 0]
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
-function getMovingSum(/* arr */) {
-  throw new Error('Not implemented');
+function getMovingSum(arr) {
+  // eslint-disable-next-line no-plusplus
+  for (let i = 1; i < arr.length; i++) {
+    // eslint-disable-next-line operator-assignment, no-param-reassign
+    arr[i] = arr[i] + arr[i - 1];
+  }
+  return arr;
 }
 
 /**
