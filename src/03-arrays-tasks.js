e96eb8eab3676bac0a1c5e014de8598501fa3569
Alexey Lagun <alexey.lagun@gmail.com>
2022-12-06 20:58:08 +0300

    feat: solution sortDigitNamesByNumericOrder

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -316,8 +316,20 @@ function getPositivesCount(arr) {
  *   [ 'nine','eight','nine','eight'] => [ 'eight','eight','nine','nine']
  *   [ 'one','one','one','zero' ]     => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  throw new Error('Not implemented');
+function sortDigitNamesByNumericOrder(arr) {
+  const numbers = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+  ];
+  return arr.sort((a, b) => numbers.indexOf(a) - numbers.indexOf(b));
 }
 
 /**
