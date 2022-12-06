6a9c814cf2604df65e2a49ff8df899da799b670d
Alexey Lagun <alexey.lagun@gmail.com>
2022-12-06 23:06:24 +0300

    feat: solution getIdentityMatrix

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -445,8 +445,8 @@ function sortCitiesArray(arr) {
  *           [0,0,0,1,0],
  *           [0,0,0,0,1]]
  */
-function getIdentityMatrix(/* n */) {
-  throw new Error('Not implemented');
+function getIdentityMatrix(n) {
+  return Array(n).fill().map((el, i) => (Array(n).fill(0).fill(1, i, i + 1)));
 }
 
 /**
